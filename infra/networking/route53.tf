c0a08905899ca9ad4b4dea453a6776bdb28c5958
Jeff Paredes <imjeffparedes@gmail.com>
2026-02-17 19:42:28 +0800

    feat(infra): add Route53 DNS records for domain, API, admin, and media subdomains

---
+++ infra/networking/route53.tf
@@ -0,0 +1,51 @@
+# A record for main domain pointing to ALB (storefront)
+resource "aws_route53_record" "main" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = false
+  }
+}
+
+# A record for API subdomain pointing to ALB (backend)
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "api.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = false
+  }
+}
+
+# A record for admin subdomain pointing to ALB (backend)
+resource "aws_route53_record" "admin" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "admin.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = false
+  }
+}
+
+# A record for media subdomain pointing to CloudFront
+resource "aws_route53_record" "media" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "media.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = module.cloudfront.cloudfront_domain_name
+    zone_id                = module.cloudfront.cloudfront_zone_id
+    evaluate_target_health = false
+  }
+}
